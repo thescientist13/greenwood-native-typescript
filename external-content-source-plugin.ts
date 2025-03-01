a8af31486013523b00000300ed2bc5909e3721b0
Owen Buckley <owenbuckley13@gmail.com>
2025-02-28 19:00:33 -0500

    patching in formal types via export maps

--- external-content-source-plugin.ts
+++ external-content-source-plugin.ts
@@ -1,10 +1,13 @@
-export const customExternalSourcePlugin = () => {
+import type { SourcePlugin } from '@greenwood/cli';
+
+export const customExternalSourcePlugin = (): SourcePlugin => {
   return {
     type: "source",
     name: "source-plugin-external-page",
     provider: () => {
       return async function () {
         return [{
+          id: "my-external-page",
           title: 'My custom external page',
           body: '<h1>My custom external page</h1>',
           route: '/external/',
--- greenwood.config.ts
+++ greenwood.config.ts
@@ -1,16 +1,29 @@
+import type { VercelAdapterOptions } from '@greenwood/plugin-adapter-vercel';
+import type { Config } from '@greenwood/cli';
 import { customExternalSourcePlugin } from "./external-content-source-plugin.ts";
 import { builtInTypeScriptPlugin } from "./built-in-typescript-plugin.ts";
+import { greenwoodPluginAdapterVercel } from '@greenwood/plugin-adapter-vercel';
+import { greenwoodPluginCssModules } from '@greenwood/plugin-css-modules';
 
 const port: number = 8181;
+const options: VercelAdapterOptions = {
+  runtime: "nodejs18.x"
+};
 
-export default {
+console.log({options});
+
+const config: Config = {
   devServer: {
     port
   },
   port,
   prerender: true,
   plugins: [
+    greenwoodPluginAdapterVercel(options),
+    greenwoodPluginCssModules(),
     builtInTypeScriptPlugin(),
     customExternalSourcePlugin()
   ]
 }
+
+export default config;
